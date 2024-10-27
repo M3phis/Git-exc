44bd4bfc17f16493c1a2b5b84f6b3c4959a11c9e
Tomer <tomera.almog9@gmail.com>
2024-10-27 20:19:21 +0200

    Limit ball size - js

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 'use strict'
 
 var gBallSize = 100
-var gBallHeight = 100
 
 function onBallClick(el) {
-  console.log(el)
   var newBallSize = gBallSize + 50
-  var newBallHeight = gBallHeight + 50
   gBallSize = newBallSize
-  gBallHeight = newBallHeight
-  el.style.width = newBallSize + 'px'
-  el.style.height = newBallHeight + 'px'
 
-  el.firstElementChild.innerText = gBallSize
+  if (gBallSize === 400) {
+    gBallSize = 100
+    el.style.width = '100px'
+    el.style.height = '100px'
+    el.firstElementChild.innerText = gBallSize
+  } else {
+    el.style.width = newBallSize + 'px'
+    el.style.height = newBallSize + 'px'
+
+    el.firstElementChild.innerText = gBallSize
+  }
 }
