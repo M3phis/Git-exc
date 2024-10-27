f24d44202463c8cefe030f274f4cb0ac387b45d7
Tomer <tomera.almog9@gmail.com>
2024-10-27 20:09:52 +0200

    Improve onBallClick()

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 'use strict'
 
-function onBallClick() {
-  console.log('The ball is clicked')
+var gBallSize = 100
+var gBallHeight = 100
+
+function onBallClick(el) {
+  console.log(el)
+  var newBallSize = gBallSize + 50
+  var newBallHeight = gBallHeight + 50
+  gBallSize = newBallSize
+  gBallHeight = newBallHeight
+  el.style.width = newBallSize + 'px'
+  el.style.height = newBallHeight + 'px'
+
+  el.firstElementChild.innerText = gBallSize
 }
